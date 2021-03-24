b43552cde22e6c515fb4bd0eac3e0637a08b1e5e
Your Name <forrest.mccrosky@gmail.com>
2021-03-24 09:18:52 -0500

    Tables exercises with Code and Questions

---
+++ tables_exercises.sql
@@ -0,0 +1,22 @@
+-- This is my file for the Table ciriculum in Sequel Pro
+
+-- Using the Employees database below.
+USE employees;
+-- Showing all the tables below.
+SHOW tables;
+-- showing/exploring employees table
+DESCRIBE employees;
+
+-- Questions 
+-- Which table(s) do you think contain a numeric type column?
+-- All the tables
+-- Which table(s) do you think contain a string type column?
+-- employees, departments, titles, and dept_emp
+-- Which table(s) do you think contain a date type column?
+-- Salaries and dept_emp_latest_date
+-- What is the relationship between the employees and the departments tables?
+-- There is no relationship
+
+
+-- This shows the SQL that creates the dept_table.
+Show create table dept_manager;
